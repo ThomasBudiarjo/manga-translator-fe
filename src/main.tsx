9b287cc5ad874e21ec7f5d6076fb72e8ef08fd59
ThomasBudiarjo <thomasbudiarjo@gmail.com>
2026-04-17 12:39:10 +0700

    Refactor ClerkProvider in main.tsx to simplify props by removing afterSignOutUrl

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,7 @@ import { ClerkProvider } from "@clerk/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ClerkProvider
-      afterSignOutUrl="/"
-      publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}
-    >
+    <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}>
       <App />
     </ClerkProvider>
   </StrictMode>,
